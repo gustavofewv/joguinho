21d03d2cb8b01d6c529a6defc0af7aaf228e5dfc
gustavofewv <gustavo.oliveira.alcantara@escola.pr.gov.br>
2026-02-12 09:36:44 -0300

    Add files via upload

---
+++ pytom/main.py
@@ -0,0 +1,26 @@
+
+print("----------")
+print("Bm vindo ao jogo")
+print("----------")
+
+
+n_secreto = 100
+entrada = int(input("Digite um número: "))
+acerto = entrada == n_secreto
+entrada_maior = entrada > n_secreto
+entrada_menor = entrada < n_secreto
+
+print(f"Você digitou o número: {entrada}")
+
+
+if(Acertou):
+    print(Você conseguiu)
+if(entrada == n_secreto):
+    print(f"Você Acertou")
+else:
+    if(entrada > n_secreto):
+        print("o numero é menor em")
+    if(entrada < n_secreto):
+        print("o numero a maior que isso")
+
+print("Fim de Jogo")
---
+++ pytom/variaveis.py
@@ -0,0 +1,10 @@
+variavel = "uma variavel não precisa ; no final"
+
+print(variavel)
+variavel2 = 500
+variavel3 = 30.5
+variavel4 = True
+print(type(variavel),type(variavel2),type(variavel3),type(variavel4))
+
+
+input("digite um valor aleatorio:")
